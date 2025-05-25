1299dba5eb36fa885bc8699fdcc3df9223c8e520
sumanth_jm <Sumanthjm2002@gmail.com>
2025-05-25 15:04:05 +0530

    update

--- program/Demo.java
+++ program/Demo.java
@@ -1,49 +1,49 @@
-import java.util.Scanner;;
-public class Demo {
+// import java.util.Scanner;;
+// public class Demo {
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n1,n2,last=0;
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n1,n2,last=0;
         
-        System.out.println("enter value for n1 and n2");
-        n1=sc.nextInt();
-        n2=sc.nextInt();
-        int aryn1[]=new int[n1];
-        int aryn2[]=new int[n2];
-        System.out.println(" enter array 1 elementes");
-        for(int i=0;i<aryn1.length;i++)
-        {
-            aryn1[i]=sc.nextInt();
-        }
-        System.out.println(" enter array 2 elementes");
+//         System.out.println("enter value for n1 and n2");
+//         n1=sc.nextInt();
+//         n2=sc.nextInt();
+//         int aryn1[]=new int[n1];
+//         int aryn2[]=new int[n2];
+//         System.out.println(" enter array 1 elementes");
+//         for(int i=0;i<aryn1.length;i++)
+//         {
+//             aryn1[i]=sc.nextInt();
+//         }
+//         System.out.println(" enter array 2 elementes");
 
-        for(int i=0;i<aryn2.length;i++)
-        {
-            aryn2[i]=sc.nextInt();
-        }
-        for(int i=0;i<aryn1.length;i++){
-            int count=0;
-            for(int j=0;j<aryn2.length;j++){
-                if (aryn1[i]==aryn2[j]) {
-                    count++;
-                }
+//         for(int i=0;i<aryn2.length;i++)
+//         {
+//             aryn2[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<aryn1.length;i++){
+//             int count=0;
+//             for(int j=0;j<aryn2.length;j++){
+//                 if (aryn1[i]==aryn2[j]) {
+//                     count++;
+//                 }
                 
-            }
-            if (count==0) {
-                System.out.print(aryn1[i]+" ");
-                last=aryn1[i];
-            }
+//             }
+//             if (count==0) {
+//                 System.out.print(aryn1[i]+" ");
+//                 last=aryn1[i];
+//             }
             
-        }
-        for(int j=0;j<aryn2.length;j++)
-        {
-            if (aryn2[j]>last) {
-                System.out.print(aryn2[j]+" ");
-            }
-        }
-        sc.close();
-    }
-}
+//         }
+//         for(int j=0;j<aryn2.length;j++)
+//         {
+//             if (aryn2[j]>last) {
+//                 System.out.print(aryn2[j]+" ");
+//             }
+//         }
+//         sc.close();
+//     }
+// }
 
 
 
@@ -92,3 +92,59 @@ public class Demo {
 //         sc.close();
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;;
+public class Demo {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1,n2,last=0;
+        
+        System.out.println("enter value for n1 and n2");
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        int aryn1[]=new int[n1];
+        int aryn2[]=new int[n2];
+        System.out.println(" enter array 1 elementes");
+        for(int i=0;i<aryn1.length;i++)
+        {
+            aryn1[i]=sc.nextInt();
+        }
+        System.out.println(" enter array 2 elementes");
+
+        for(int i=0;i<aryn2.length;i++)
+        {
+            aryn2[i]=sc.nextInt();
+        }
+        for(int i=0;i<aryn1.length;i++){
+            if(aryn1[i]%2==0){
+            int count=0;
+            for(int j=0;j<aryn2.length;j++){
+                if (aryn1[i]==aryn2[j]) {
+                    count++;
+                }
+                
+            }
+            if (count==1) {
+                System.out.print(aryn1[i]+" ");
+                last=aryn1[i];
+            }
+        }
+        }
+        
+        sc.close();
+    }
+}
+
+
